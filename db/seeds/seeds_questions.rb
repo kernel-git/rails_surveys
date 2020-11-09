0a161d396ca2ecb8cda36835f40f39d440c560d5
boss078 <36530564+boss078@users.noreply.github.com>
2020-11-09 17:51:01 +0300

    Minor fixes in seeds

--- db/seeds/seeds_questions.rb
+++ db/seeds/seeds_questions.rb
@@ -77,21 +77,6 @@ questions[8, 9].each { |e| e.question_group = QuestionGroup.find_by(label: 'Soci
 questions[10].question_group = QuestionGroup.find_by(label: 'Social: Freedom') }
 questions[11..13].each { |e| e.question_group = QuestionGroup.find_by(label: 'Social: Medical') }
 
-questions[0].answers = Answer.where(id: 1..2)
-questions[1].answers = Answer.where(id: 3..6)
-questions[2].answers = Answer.where(id: 7..8)
-questions[3].answers = Answer.where(id: 9..14)
-questions[4].answers = Answer.where(id: 15..17)
-questions[5].answers = Answer.where(id: 18..19)
-questions[6].answers = Answer.where(id: 20..21)
-questions[7].answers = Answer.where(id: 22..25)
-questions[8].answers = Answer.where(id: 26..28)
-questions[9].answers = Answer.where(id: 29..30)
-questions[10].answers = Answer.where(id: 31..35)
-questions[11].answers = Answer.where(id: 36..38)
-questions[12].answers = Answer.where(id: 39..40)
-questions[13].answers = Answer.where(id: 41..43)
-
 fake_user = User.new
 questions.each do |e|
     e.answers.each { |answ| answ.user = fake_user }
--- db/seeds/seeds_users.rb
+++ db/seeds/seeds_users.rb
@@ -42,57 +42,6 @@ users[7].segments << Segment.find_by(label: 'Teacher')
 users[7].segments << Segment.find_by(label: 'Technical')
 users[7].segments << Segment.find_by(label: 'Management')
 
-users[0].answers << Answer.find(1)
-users[0].answers << Answer.find(7)
-users[0].answers << Answer.find(3)
-users[0].answers << Answer.find(9)
-users[0].answers << Answer.find(15)
-
-users[1].answers << Answer.find(2)
-users[1].answers << Answer.find(4)
-users[1].answers << Answer.find(8)
-users[1].answers << Answer.find(38)
-users[1].answers << Answer.find(43)
-users[1].answers << Answer.find(34)
-
-users[2].answers << Answer.find(5)
-users[2].answers << Answer.find(16)
-users[2].answers << Answer.find(18)
-users[2].answers << Answer.find(20)
-users[2].answers << Answer.find(22)
-users[2].answers << Answer.find(26)
-users[2].answers << Answer.find(35)
-
-users[3].answers << Answer.find(6)
-users[3].answers << Answer.find(10)
-users[3].answers << Answer.find(17)
-users[3].answers << Answer.find(19)
-users[3].answers << Answer.find(21)
-users[3].answers << Answer.find(23)
-users[3].answers << Answer.find(27)
-
-users[4].answers << Answer.find(11)
-users[4].answers << Answer.find(24)
-users[4].answers << Answer.find(28)
-
-users[5].answers << Answer.find(12)
-users[5].answers << Answer.find(25)
-users[5].answers << Answer.find(29)
-users[5].answers << Answer.find(31)
-
-users[6].answers << Answer.find(13)
-users[6].answers << Answer.find(30)
-users[6].answers << Answer.find(32)
-users[6].answers << Answer.find(36)
-users[6].answers << Answer.find(39)
-users[6].answers << Answer.find(41)
-
-users[7].answers << Answer.find(14)
-users[7].answers << Answer.find(33)
-users[7].answers << Answer.find(37)
-users[7].answers << Answer.find(40)
-users[7].answers << Answer.find(42)
-
 begin
     users.each(&:save!)
 rescue => e
@@ -104,3 +53,22 @@ rescue => e
     puts '~~~~~~~~~~~~~~~~~~~~~~~~~'
 end
 
+Question.all.each do |question|
+    Faker::Number.unique.clear
+    rand(0..8).times do
+        begin
+            Answer.create!({
+                answer_val: rand(27).to_f / 2,
+                question_id: question.id,
+                user_id: Faker::Number.unique.within(range: 1..8)
+            })
+        rescue => e
+            puts '--->EXEPTION DURING SAVE<---'
+            puts "Exeption type: #{e.class.name}"
+            puts "Exeption message: #{e.message}"
+            puts '~~~~~~~Stack trace~~~~~~~'
+            e.backtrace.each { |line| puts line }
+            puts '~~~~~~~~~~~~~~~~~~~~~~~~~'
+        end
+    end
+end
